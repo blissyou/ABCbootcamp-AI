f5450c203179b6d51eedab67e7f4a3577c2bc55e
원형 <147372294+blissyou@users.noreply.github.com>
2024-07-23 16:24:37 +0900

    AIcodes

---
+++ Test/code13.py
@@ -0,0 +1,7 @@
+import numpy as np
+X = np.array([['Korea', 44, 7200], ['Japan', 27, 4800], ['China', 30, 6100]])
+from sklearn.preprocessing import OneHotEncoder
+onehotencoder = OneHotEncoder() # 원하는 열을 뽑아서 2차원 배열로 만들어서 전달하여야 한다.
+XX = onehotencoder.fit_transform(X[:,0].reshape(-1,1)).toarray()
+print(XX)
+X = np.delete(X, [0], axis=1) # 0번째 열 삭제 X = np.c
